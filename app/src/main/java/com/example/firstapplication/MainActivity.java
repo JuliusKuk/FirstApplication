e7b1cdea9a35f503b44bd4d0566eb41963bf1e67
JuliusKuk <kukenys.julius@gmail.com>
2026-09-20 20:07:39 +0300

    idetas antras mygtukas spalvos pakeitimui

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firstapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,13 +35,18 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        Random r = new Random();
+
         Button button = findViewById(R.id.button);
         TextView tekstas = findViewById(R.id.textView);
         EditText editintasTekstas = findViewById(R.id.editTextText);
-
+        Button button1 = findViewById(R.id.button2);
         button.setOnClickListener(v -> {
 
             tekstas.setText(editintasTekstas.getText());
+        });
+        button1.setOnClickListener(v -> {
+            tekstas.setTextColor(Color.rgb(r.nextInt(255),r.nextInt(255),r.nextInt(255)));
         });
     }
 
